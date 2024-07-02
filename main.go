25a0e4559f7b03ab76c53f5b26c0eed7446a53c2
Tocyuki <tocguitar1@gmail.com>
2024-07-02 22:03:31 +0900

    TODOアプリの項目を表現するStructを作成

--- main.go
+++ main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/uptrace/bun"
 )
+
+type Todo struct {
+	bun.BaseModel `bun:"table:todos,alias:t"`
+
+	ID        int64     `bun:"id,pk,autoincrement"`
+	Content   string    `bun:"content,notnull"`
+	Done      bool      `bun:"done"`
+	Until     time.Time `bun:"until,nullzero"`
+	CreatedAt time.Time
+	UpdatedAt time.Time `bun:"nullzero"`
+	DeletedAt time.Time `bun:"soft_delete,nullzero"`
+}
 
 func main() {
 	e := echo.New()
