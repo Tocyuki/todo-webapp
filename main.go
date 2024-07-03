f960a55cc27225b724dff849e388cfe672278ae4
Tocyuki <tocguitar1@gmail.com>
2024-07-03 22:32:57 +0900

    JSTを付与してパースすることで日本時間で扱えるようにする関数を追加

--- main.go
+++ main.go
@@ -29,6 +29,25 @@ type Todo struct {
 	DeletedAt time.Time `bun:"soft_delete,nullzero"`
 }
 
+type Data struct {
+	Todos  []Todo
+	Errors []error
+}
+
+func customFunc(todo *Todo) func([]string) []error {
+	return func(values []string) []error {
+		if len(values) == 0 || values[0] == "" {
+			return nil
+		}
+		dt, err := time.Parse("2006-01-02T15:04 MST", values[0]+" JST")
+		if err != nil {
+			return []error{echo.NewBindingError("until", values[0:1], "failed to decode time", err)}
+		}
+		todo.Until = dt
+		return nil
+	}
+}
+
 func main() {
 	sqldb, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
